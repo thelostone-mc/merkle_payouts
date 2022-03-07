da2e567d4000dc15322a81e2fab94533ee940b78
Andrea Franz <andrea@gravityblast.com>
2022-03-07 12:18:32 +0100

    add inject-merkle-claims script

---
+++ scripts/ingest-merkle-claims.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import APIClient from '../utils/api-client';
+import fs from 'fs';
+import path from 'path';
+import { confirmContinue } from '../utils/scripts-utils';
+
+async function main() {
+  const apiBaseURL = process.env.API_BASE_URL;
+  const apiToken = process.env.TOKEN;
+  const grantPayoutPk = process.env.GRANT_PAYOUT_PK;
+
+  const rawData = fs.readFileSync(path.join(__dirname, './output.json'));
+  const claims = JSON.parse(rawData.toString())
+
+  await confirmContinue({
+    'url': apiBaseURL,
+    'token': apiToken,
+    'grantPayoutPk': grantPayoutPk,
+    'claimsCount': claims.length,
+  });
+
+  const c = new APIClient(axios, apiBaseURL!, apiToken!);
+  const resp = await c.injestMerkleClaims({
+    grant_payout_pk: grantPayoutPk,
+    claims,
+  });
+
+  console.log(resp)
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
